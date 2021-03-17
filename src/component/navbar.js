04af2bb627ff604e29abf451807d10c1f737cf78
muhammadtayyabgujjar <gujjartayyab9494@gmail.com>
2021-03-17 11:34:00 +0500

    gatsby

---
+++ src/component/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar=()=>{
+
+
+    return(
+    <div>
+         <link to="/">Home</link>
+          <link to="./about">About</link>
+          <link to="./Contact">Contact</link>
+    </div>
+    )
+}
+export default Navbar
